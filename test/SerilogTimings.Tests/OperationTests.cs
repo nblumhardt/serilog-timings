76ccd7d439e08e267f9e53a933a6d9ba21d91893
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-04-20 18:07:51 +1000

    First test

---
+++ test/SerilogTimings.Tests/OperationTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace SerilogTimings.Tests
+{
+    public class OperationTests
+    {
+        [Fact]
+        public void HelloTests()
+        {
+            Assert.True(true);
+        }
+    }
+}
